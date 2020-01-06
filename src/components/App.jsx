a7af14544eeb3265f3c865dfd38b091288e41d1c
Akshay Anand <akshay.anand12@gmail.com>
2020-01-06 16:53:21 +0800

    Use file loader to render svg and png images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { Container } from './ui';
+import FunnyFace from '../images/funny-face.jpg'
+import Microphone from '../images/microphone.svg'
 
 const Header = () =>
     (<div style={{ padding: "50px" }}>
         Experimenting with webpack
     </div>)
 
+const style = {
+    width: 100,
+    height: 100
+}
+
 const App = () => {
     return (
         <Container>
             <Header />
             {"Hello"}
+            <Container>
+                <img style={style} src={FunnyFace} />
+                <img style={style} src={Microphone} />
+            </Container>
         </Container>
     )
 }
